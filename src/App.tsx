31a58a8673f77d272b99fc3c330aa323fe8f857f
psandesh64 <psandesh64@gmail.com>
2025-12-03 15:34:52 +0545

    tokens deleted

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-      <AppConfigProvider>
+      <AppConfigProvider isDarkMode={ true }>
         <div style={{display: 'flex', gap:10}}>
             <Link to="/">Home</Link>   
             <Link to="my-page">MyPage</Link>   
